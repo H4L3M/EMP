9e16012e40a72a1f53cd2084b8f8ba0323008e24
H4L3M <halem.dev@gmail.com>
2021-03-03 14:36:43 +0100

    Initial commit

--- app/src/main/java/com/mowakib/emp/UrlSources.kt
+++ app/src/main/java/com/mowakib/emp/UrlSources.kt
@@ -1,21 +1,21 @@
 package com.mowakib.emp
 
 
-private const val MPD = "https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd"
-private const val M3U8 =
+internal const val MPD = "https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd"
+internal const val M3U8 =
     "https://cdnamd-hls-globecast.akamaized.net/live/ramdisk/al_aoula_inter/hls_snrt/al_aoula_inter.m3u8"
-private const val MP3 = "https://storage.googleapis.com/exoplayer-test-media-0/play.mp3"
-private const val OGG = "https://storage.googleapis.com/exoplayer-test-media-1/ogg/play.ogg"
-private const val MP4_S =
+internal const val MP3 = "https://storage.googleapis.com/exoplayer-test-media-0/play.mp3"
+internal const val OGG = "https://storage.googleapis.com/exoplayer-test-media-1/ogg/play.ogg"
+internal const val MP4_S =
     "https://playready.directtaps.net/smoothstreaming/SSWSS720H264PR/SuperSpeedway_720.ism/Manifest"
-private const val MP4 = "https://html5demos.com/assets/dizzy.mp4"
-private const val MKV =
+internal const val MP4 = "https://html5demos.com/assets/dizzy.mp4"
+const val MKV =
     "https://storage.googleapis.com/exoplayer-test-media-1/mkv/android-screens-lavf-56.36.100-aac-avc-main-1280x720.mkv"
-private const val MPEG_TS =
+internal const val MPEG_TS =
     "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/gear1/fileSequence0.ts"
-private const val AAC =
+internal const val AAC =
     "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_4x3/gear0/fileSequence0.aac"
-private const val WEBM =
+internal const val WEBM =
     "https://storage.googleapis.com/wvmedia/2019/clear/av1/24/webm/llama_av1_480p_400.webm"
-private const val FLAC = "https://storage.googleapis.com/exoplayer-test-media-1/flac/play.flac"
-private const val FLV = "https://vod.leasewebcdn.com/bbb.flv?ri=1024&rs=150&start=0"
+internal const val FLAC = "https://storage.googleapis.com/exoplayer-test-media-1/flac/play.flac"
+internal const val FLV = "https://vod.leasewebcdn.com/bbb.flv?ri=1024&rs=150&start=0"
